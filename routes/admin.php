142d642657c680c5675927f45cea339e43319614
Samuel Caitano <smlcaitano@gmail.com>
2022-06-20 17:58:31 -0300

    new controller routes of upgrade laravel

--- routes/admin.php
+++ routes/admin.php
@@ -1,46 +1,58 @@
 <?php
+
+use App\Http\Controllers\Admin\PageMenuController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\SectionMenuController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
+
+/**
+ * Se um grupo de rotas utiliza o mesmo controller , você pode usar o controller método para definir o controller comum para todas as rotas dentro do grupo. Então, ao definir as rotas, você só precisa fornecer o método do controller que eles invocam   
+ */
 
 Route::get('/', 'HomeController@index')->name('admin.dashboard');  
   
-Route::group([ 'prefix' => 'userProfile' ], function() {
-  $ctrll = 'UserProfileController@';
-	Route::get('', "{$ctrll}list");
-	Route::delete('{id}', "{$ctrll}disable");
-	Route::patch('{id}', "{$ctrll}enable");
-	Route::put('{id}', "{$ctrll}save");
+Route::controller(UserProfileController::class)->group( function () {
+	Route::group([ 'prefix' => 'userProfile' ], function() { 
+		Route::get('', "list");
+		Route::delete('{id}', "disable");
+		Route::patch('{id}', "enable");
+		Route::put('{id}', "save");
+	});
 });
 
-Route::group([ 'prefix' => 'user' ], function() {
-  $ctrll = 'UserController@';
-	Route::get('', "{$ctrll}list");
-	Route::delete('{id}', "{$ctrll}disable");
-	Route::patch('{id}', "{$ctrll}enable");
-	Route::put('{id}', "{$ctrll}save");
+Route::controller(UserController::class)->group( function () {
+	Route::group([ 'prefix' => 'user' ], function() { 
+		Route::get('', "list");
+		Route::delete('{id}', "disable");
+		Route::patch('{id}', "enable");
+		Route::put('{id}', "save");
+	});
 });
 
-Route::group([ 'prefix' => 'sectionMenu' ], function() {
-  $ctrll = 'SectionMenuController@';
-	Route::get('', "{$ctrll}list");
-	Route::delete('{id}', "{$ctrll}disable");
-	Route::patch('{id}', "{$ctrll}enable");
-	Route::put('{id}', "{$ctrll}save");
+Route::controller(SectionMenuController::class)->group( function () {
+	Route::group([ 'prefix' => 'sectionMenu' ], function() { 
+		Route::get('', "list");
+		Route::delete('{id}', "disable");
+		Route::patch('{id}', "enable");
+		Route::put('{id}', "save");
+	});
 });
 
-Route::group([ 'prefix' => 'pageMenu' ], function() {
-  $ctrll = 'PageMenuController@';
-	Route::get('', "{$ctrll}list");
-	Route::delete('{id}', "{$ctrll}disable");
-	Route::patch('{id}', "{$ctrll}enable");
-	Route::put('{id}', "{$ctrll}save");
+Route::controller(PageMenuController::class)->group( function () {
+	Route::group([ 'prefix' => 'pageMenu' ], function() { 
+		Route::get('', "list");
+		Route::delete('{id}', "disable");
+		Route::patch('{id}', "enable");
+		Route::put('{id}', "save");
+	});
 });
 
-Route::group([ 'prefix' => 'permission' ], function() {
-  $ctrll = 'PermissionController@';
-	Route::get('', "{$ctrll}list");
-	Route::delete('{id}', "{$ctrll}disable");
-	Route::patch('{id}', "{$ctrll}enable");
-	Route::put('{id}', "{$ctrll}save");
+Route::controller(PermissionController::class)->group( function () {
+	Route::group([ 'prefix' => 'permission' ], function() { 
+		Route::get('', "list");
+		Route::delete('{id}', "disable");
+		Route::patch('{id}', "enable");
+		Route::put('{id}', "save");
+	});
 });
-
-
